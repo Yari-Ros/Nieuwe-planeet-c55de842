57fae89dcbde1219edd277c385f017b9fa7e402c
Yari-Ros <2118088@talnet.nl>
2020-02-11 11:36:21 +0100

    Uitwerking van de exercise

---
+++ exercise.sql
@@ -0,0 +1,29 @@
+-- CREATE DATABASE Sterrenstelsel;
+USE sterrenstelsel;
+-- CREATE TABLE Planeten (name VARCHAR(20));
+-- INSERT INTO planeten (name) VALUES('Mercurius');
+-- INSERT INTO planeten (name) VALUES('Aarde');
+-- INSERT INTO planeten (name) VALUES('Mars');
+SHOW TABLES;
+-- describe planeten;
+-- truncate planeten;	
+-- CREATE TABLE `informatie` (
+-- `naam` varchar(15), 
+-- `diameter` int(10), 
+-- `afstand tot de zon` int(15),
+-- `massa tov de aarde` int(10)
+-- );
+
+-- INSERT INTO informatie (naam, diameter, `afstand tot de zon`, `massa tot de aarde`) VALUES ('Venus', 12104, 108208930, 0.9);
+-- INSERT INTO informatie (naam, diameter, `afstand tot de zon`, `massa tot de aarde`) VALUES ('Jupiter', 142.984, 778412010, 318);
+-- INSERT INTO informatie (naam, diameter, `afstand tot de zon`, `massa tot de aarde`) VALUES ('Uranus', 51118, 287097220, 15);
+
+-- alter table informatie MODIFY `naam` varchar(15) NOT NULL;
+-- alter table informatie MODIFY `diameter` int(10) NOT NULL;
+-- alter table informatie MODIFY `afstand tot de zon` int(15) NOT NULL;
+-- alter table informatie MODIFY `massa tov de aarde` int(10) NOT NULL;
+-- alter table informatie MODIFY `bezoek_datum` DATE;
+
+ALTER TABLE informatie ADD id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY;
+
+describe informatie;
